108a53fe9f280145222ab15f792699a0fd830686
michal <michal@Ubuntu-aspire>
2017-11-05 18:09:58 +0100

    Klasa ulamek

---
+++ Ulamek/src/testUlamek.java
@@ -0,0 +1,15 @@
+public class testUlamek {
+    
+    
+    public static void main(String[] args) {
+        Ulamek pierwszy,drugi;
+        pierwszy = new Ulamek(1,2);
+        drugi = new Ulamek(3,4);
+        System.out.println(pierwszy);
+        System.out.println(drugi);
+        double x = pierwszy.rozwDziesietne();
+        System.out.println(x);
+
+    }   
+    
+}
